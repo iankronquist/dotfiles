cf7c09d4d18fba55bf793e944813ebc9f75c074f
Ian Kronquist <iankronquist@teleport.com>
2014-07-25 17:32:24 -0700

    Add script to automatically install stuff

---
+++ installdotfiles.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# A little hacky, but a lot simpler than some I've seen
+
+if [[ $(uname) == "Darwin" ]]; then
+	echo "Installing OSX specific configuration files"
+	echo "Installing dev tools"
+	echo "Installing Xcode"
+	xcode-select --install
+	echo "Installing Homebrew"
+	curl https://raw.github.com/Homebrew/homebrew/go/install > brewinstall
+	cat brewinstall
+	echo "Should I run this script?"
+	read CONFIRM
+	if [[ $CONFIRM == "yes" ]]; then 
+		chmod +x brewinstall
+		./brewinstall
+		rm brewinstall
+		echo "Installing Homebrew packages"
+		brew install $(cat osx/brewfiles)
+	fi
+	echo "Copying plists"
+	cp osx/com.apple.Terminal.plist ~/Library/Preferences/com.apple.Terminal.plist
+elif [[ $(uname) == "Linux" ]]; then
+	echo "Installing Linux specific configuration files"
+	mv linux/.* .
+else
+	echo "Unknown system. exiting"
+	exit
+fi
+
+mkdir $HOME/git
+
+for file in "$(ls -a)"; do
+	if [[ $file != "osx" && $file != "linux" && $file != "$0" ]]; then
+		ln $file $HOME/$file
+	fi
+done
