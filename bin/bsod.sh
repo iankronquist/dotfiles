0161a8023927109a37ea5231ee3510b7d96a8aa7
Ian Kronquist <iankronquist@gmail.com>
2017-07-14 17:09:29 -0400

    Things I wrote

--- bin/bsod.sh
+++ bin/bsod.sh
@@ -3,13 +3,25 @@
 set -e
 #set -x
 
+if [[ $1 =~ 'clean' ]]; then
+	echo cleaning ~/gg/bsod/*
+	rm -rf ~/gg/bsod/*
+	exit
+fi
+
 
 cd ~
 
-#if ! [ -d /Volumes/Logs/dumps ]; then
-#	mkdir -p /Volumes/Logs
-#	mount_smbfs //synology.barkly.local/Logs /Volumes/Logs
-#fi
+if ! [ -d /Volumes/Logs/dumps ]; then
+	mkdir -p /Volumes/Logs
+
+	mount_smbfs //synology.barkly.local/Logs /Volumes/Logs
+	if [[ $? != 0 ]]; then
+		echo "couldn't mount dumps"
+		rmdir /Volumes/Logs
+		exit
+	fi
+fi
 
 recent_bsod=$(ls -Art /Volumes/Logs/dumps/ | grep "$1$" | tail -n 1)
 
@@ -25,7 +37,7 @@ cp /Volumes/Logs/dumps/$recent_bsod/*.zip .
 unzip *.zip
 
 if [ -e memory.dmp ]; then
-	grep -a '\[Barkly:0x[a-z0-9]\+\]' memory.dmp | strings | sort > panic.txt
+	grep -a '\[Barkly-VMM:0x[a-z0-9]\+\]' memory.dmp | strings | LC_ALL='C' sort > panic.txt
 	cat panic.txt
 
 	~/gg/RV-Tools/Debugging/PanicParser.py panic.txt > parsed.txt
@@ -34,4 +46,6 @@ if [ -e memory.dmp ]; then
 	bugcheckinfo.py memory.dmp
 else
 	echo "No memory dump. Go look in logs."
+	read
+	vim -p logs/*log
 fi
