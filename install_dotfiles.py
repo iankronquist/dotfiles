98b7bb9f6d0815ccce5e69104a24e82fd82a5f97
Ian Kronquist <i_kronquist@apple.com>
2023-07-13 13:17:28 -0700

    make install dotfiles more resilient

--- install_dotfiles.py
+++ install_dotfiles.py
@@ -19,8 +19,19 @@ links = {
 
 here = os.path.dirname(os.path.abspath(__file__))
 
+#os.chdir(os.path.expanduser('~'))
 for (source_file, symlink) in links.items():
 	source_file = os.path.realpath(os.path.join(here, source_file))
-	symlink = os.path.realpath(os.path.expanduser(symlink))
+	print(symlink)
+	symlink = os.path.expanduser(symlink)
+	print(symlink)
 	print(source_file, symlink)
+	if os.path.islink(symlink):
+		if os.readlink(symlink) == source_file:
+			print('link exists, continuing')
+			continue
+	elif os.path.exists(symlink):
+		old = symlink + '.old'
+		print('file', symlink, 'exists, renaming to', old)
+		os.rename(symlink, old)
 	os.symlink(source_file, symlink)
